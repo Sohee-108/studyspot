85b7d3f2551eb1aa46225d3779632646ae0a86f7
ddohing <dnflwlq004@naver.com>
2021-11-26 21:09:35 +0900

    feature: 일정추가 화면 구현

--- view/Main.js
+++ view/Main.js
@@ -3,7 +3,6 @@ import React from 'react';
 import MainView from '../components/MainView';
 import Timer from './StopWatch';
 import Schedule from './Schedule';
-import ScheduleAdd from './ScheduleAdd';
 import Setting from './Setting';
 import Profile from './Profile';
 
@@ -19,7 +18,6 @@ const Main = (navigation) => {
       <MainStack.Screen name="MainView" component={MainView} options={{title: "스터디스팟"}} />
       <MainStack.Screen name="Timer" component={Timer} options={{title: "공부시간 측정하기"}} />
       <MainStack.Screen name="Schedule" component={Schedule} options={{title: "일정관리"}} />
-      <MainStack.Screen name="ScheduleAdd" component={ScheduleAdd} options={{title: "일정추가"}} />
       <MainStack.Screen name="Setting" component={Setting} options={{title: "설정"}} />
       <MainStack.Screen name="Profile" component={Profile} options={{title: "프로필"}} />
       
--- view/Schedule.js
+++ view/Schedule.js
@@ -1,14 +1,20 @@
 import React, { Component, useState } from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, Button} from 'react-native';
 
 import styled from 'styled-components';
 import { useNavigation } from '@react-navigation/native';
 import {Agenda} from 'react-native-calendars';
+import Modal from "react-native-modal";
 
 // #region styled-component 부분
 
-const CenteredView = styled.SafeAreaView`
-    flex: 1;
+const CenteredView1 = styled.SafeAreaView`
+    flex:1;
+`;
+
+const CenteredView2 = styled.SafeAreaView`
+    align-items: center;
+    justify-content: center;
 `;
 
 const ButtonView = styled.View`
@@ -16,27 +22,79 @@ const ButtonView = styled.View`
     margin-right: 20px;
 `;
 
-const AddButton = styled.TouchableOpacity`
-    width:60px;
-    height:45px;  
+const AddButton1 = styled.TouchableOpacity`
+    width:70px;
+    height:50px;  
     align-items: center;
     justify-content: center;
     margin-right:5%;
+    margin-bottom: 10%;
     border-radius: 10px;
-    background-color: #D4F4FA;
+    background-color: #001EC9;
 `;
 
 const AddText = styled.Text`
-    font-size: 20;
+    font-size: 23px;
+    color: white;
+`;
+const AddButton2 = styled.TouchableOpacity`
+    width:200px;
+    height:50px;  
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    background-color: #001EC9;
+`;
+
+const AddValue = styled.TextInput`
+    background-color: white;
+    font-size: 20px;
+    width:80%;
+    height:7%;
+    border-radius: 30px;
+    padding: 15px;
+    margin-bottom: 30px;
 `;
 
 // #endregion
 
+
 const Schedule = () => {
     const navigation = useNavigation();
 
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+    };
+
+    const [day, setDay] = useState("");
+    const getDay = (day) => {
+        setDay(day);
+    }
+    const [time, setTime] = useState("");
+    const getTime = (time) => {
+        setTime(time);
+    }
+    const [name, setName] = useState("");
+    const getName = (name) => {
+        setName(name);
+        console.log(setName);
+    }
+    const [content, setContent] = useState("");
+    const getContent = (content) => {
+        setContent(content);
+    }
+
+    const minCalendar = () => {
+
+        return(
+            <View><Calendar></Calendar></View>
+        )
+    }
+
     return (
-        <CenteredView>
+        <CenteredView1>
             <Agenda style={{}} 
             items={{
                 '2021-11-23': [{name: 'item 2 - any js object'}],
@@ -50,10 +108,21 @@ const Schedule = () => {
                     <Text>{items.name}</Text>
                 </View>);}}/>
 
-            <ButtonView onPress={{Add}}>
-                <AddButton onPress={()=> navigation.navigate('ScheduleAdd')}><AddText>추가</AddText></AddButton>
+            <ButtonView>
+                <AddButton1 onPress={toggleModal}><AddText>추가</AddText></AddButton1>
+                <Modal isVisible={isModalVisible}>
+                <CenteredView2 style={{flex:1,backgroundColor:''}}>
+                    <AddValue placeholder="날짜를 입력하세요. ex)2021-01-03" value={day} onChange={setDay}></AddValue>
+                    <TouchableOpacity style={{backgroundColor:'black'}} onPress={minCalendar}><Text>달력</Text></TouchableOpacity>
+                    <AddValue placeholder="시간을 입력하세요. ex)17:00~20:00" value={time} onChange={setTime}></AddValue>
+                    <AddValue placeholder="일정명을 입력하세요." title="name" value={name} onChange={setName}></AddValue>
+                    <AddValue placeholder="일정내용을 입력하세요." value={content} onChange={setContent}></AddValue>
+                    <AddButton2 title="back" onPress = {toggleModal}>
+                    <AddText>일정 추가</AddText></AddButton2>
+                </CenteredView2>
+                </Modal>
             </ButtonView>
-        </CenteredView>
+        </CenteredView1>
     );
 }
 
