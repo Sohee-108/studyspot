7d406dbf0c8516cccaf1964cab61f9f6cf35962b
ddohing <dnflwlq004@naver.com>
2021-11-29 13:57:59 +0900

    feature: 문의하기 화면 구현

--- view/QNA.js
+++ view/QNA.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { Text, View } from "react-native";
+import styled from "styled-components";
+
+const CenteredView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IntroText = styled.Text`
+  font-size: 20;
+  padding: 5px;
+`;
 
 const QNA = () => {
-  return <View></View>;
+  return (
+    <CenteredView>
+      <IntroText>제작자 @_ddohing</IntroText>
+      <IntroText>이메일 dnflwlq004@naver.com</IntroText>
+    </CenteredView>
+  );
 };
 export default QNA;
